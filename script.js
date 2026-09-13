c5dad960bcd33af67275a9d75442c9916f1f2ca7
Silver-G-Eng <s.guillen.eng@hotmail.com>
2026-09-13 17:19:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+const gallery = document.getElementById('gallery');
+ 
+function updateScrollHint() {
+  const atEnd = gallery.scrollLeft + gallery.clientWidth >= gallery.scrollWidth - 5;
+  gallery.classList.toggle('at-end', atEnd);
+}
+ 
+if (gallery) {
+  gallery.addEventListener('scroll', updateScrollHint);
+  window.addEventListener('resize', updateScrollHint);
+  updateScrollHint();
+}
